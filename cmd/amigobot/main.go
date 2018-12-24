531da5a2708766b2a09f8b380b07520239dc824d
Ryan Miville <ryan.miville@terminus.com>
2018-12-24 11:16:13 -0500

    move handlers into separate file

--- cmd/amigobot/main.go
+++ cmd/amigobot/main.go
@@ -9,29 +9,8 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/ryanmiville/amigobot"
-	"github.com/ryanmiville/amigobot/decide"
-	"github.com/ryanmiville/amigobot/greet"
 	"github.com/ryanmiville/amigobot/help"
-	"github.com/ryanmiville/amigobot/mfp/cals"
-	"github.com/ryanmiville/amigobot/mfp/htmlparse"
-	"github.com/ryanmiville/amigobot/mfp/macros"
-	"github.com/ryanmiville/amigobot/remindme"
-	"github.com/ryanmiville/amigobot/spoiler"
-	"github.com/ryanmiville/amigobot/yn"
 )
-
-//Handlers is the list of MessageHandlers that will be checked for every message
-//sent in the channel (except the ones amigobot sends itself)
-var Handlers = []amigobot.Handler{
-	&cals.Handler{Fetcher: htmlparse.Fetcher{}},
-	&macros.Handler{Fetcher: htmlparse.Fetcher{}},
-	&yn.Handler{},
-	&greet.Handler{},
-	&remindme.Handler{},
-	&decide.Handler{},
-	&spoiler.Handler{},
-}
 
 // Variables used for command line parameters
 var (

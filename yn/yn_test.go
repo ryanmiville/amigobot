8e673d0484783e7387cbdca1db63e5efd95dd6e6
Ryan Miville <ryan.miville@terminus.com>
2018-12-24 10:03:00 -0500

    refactored tests

--- yn/yn_test.go
+++ yn/yn_test.go
@@ -4,38 +4,31 @@ import (
 	"testing"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/ryanmiville/amigobot/mock"
+	"github.com/ryanmiville/amigobot/amigobotfakes"
 )
 
 func TestGreet(t *testing.T) {
 	h := Handler{}
 	actual := &discordgo.Message{}
-	s := &mock.Session{
-		ChannelMessageSendFn: func(channelId, content string) (*discordgo.Message, error) {
-			actual.Content = content
-			actual.ChannelID = channelId
-			return actual, nil
-		},
-		MessageReactionAddFn: func(channelID, messageID, emojiID string) error {
-			reaction := &discordgo.MessageReactions{
-				Count: 1,
-				Me:    true,
-				Emoji: &discordgo.Emoji{ID: emojiID},
-			}
-			actual.Reactions = append(actual.Reactions, reaction)
-			return nil
-		},
+	s := &amigobotfakes.FakeSession{}
+	s.ChannelMessageSendStub = func(channelID, content string) (*discordgo.Message, error) {
+		actual.Content = content
+		return actual, nil
+	}
+	s.MessageReactionAddStub = func(channelID, messageID, emojiID string) error {
+		reaction := &discordgo.MessageReactions{
+			Count: 1,
+			Me:    true,
+			Emoji: &discordgo.Emoji{ID: emojiID},
+		}
+		actual.Reactions = append(actual.Reactions, reaction)
+		return nil
 	}
 	h.Handle(s, &discordgo.MessageCreate{
 		Message: &discordgo.Message{
-			Content:   "?yn Does Cody smell good?",
-			ChannelID: "11390",
+			Content: "?yn Does Cody smell good?",
 		},
 	})
-
-	if actual.ChannelID != "11390" {
-		t.Errorf("Expected ChannelID: 11390 but received %v", actual.ChannelID)
-	}
 	if actual.Content != "@everyone Does Cody smell good?" {
 		t.Errorf("Expected Content: '@everyone Does Cody smell good?' but received %v", actual.Content)
 	}
